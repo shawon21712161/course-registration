fdf46754fbe81a8170b13f4503321ff68b3d850d
shawon21712161 <sshawon19@gmail.com>
2023-11-25 23:15:06 +0600

    price and credit hour update

--- my-course-registration/src/assets/List/List.jsx
+++ my-course-registration/src/assets/List/List.jsx
@@ -1,11 +1,27 @@
-const List = ({list}) => {
+import { useEffect } from "react";
+
+const List = ({credit,setcredit,price,setprice,list}) => {
     const {courseName} = list
+
+    useEffect(()=>{
+        setprice(price+list.price)
+    },[])
+
+    useEffect(()=>{
+        setcredit(credit+list.credit)
+    },[])
     return (
-        <div className="pt-2">
+        <div>
+
+        
+        <div className="pt-2 border-b-2">
             {/* <h3 className="font-semibold">{courseName}</h3> */}
             
-                <li className="text-justify">{courseName}</li>
+           
+                <li className="text-justify font-medium">{courseName}</li>
+
             
+        </div>
         </div>
     );
 };
--- my-course-registration/src/assets/Lists/Lists.jsx
+++ my-course-registration/src/assets/Lists/Lists.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
 import List from "../List/List";
 
 const Lists = ({lists}) => {
+    const [price, setprice] = useState(0)
+    const [credit, setcredit] = useState(0)
     return (
         <div className="w-1/3 m-4 mt-10 min-h-min rounded-lg p-4 text-center bg-slate-200">
-            <h2 className=" font-semibold text-2xl">Course Lists: {lists.length}</h2>
+            <h2 className=" font-semibold text-2xl text-left">Course Name </h2>
             <hr />
-            <div className="pt-2">
+            <div className="py-2 border-b-2 border-b-gray-400">
                 {
-                    lists.map(list=> <List key={list.id} list={list}></List>)
+                    lists.map(list=> 
+                    <List 
+                    key={list.id} 
+                    price={price} 
+                    setprice={setprice}
+                    list={list}
+                    credit={credit}
+                    setcredit={setcredit}
+                    ></List>)
                 }
             </div>
+            <h4 className="text-lg font-semibold py-2">Total Credit Hour : {credit}</h4>
+
+            <h4 className="text-lg font-semibold py-2">Total Price : {price} USD</h4>
         </div>
     );
 };
